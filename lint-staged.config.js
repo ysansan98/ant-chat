8c6941b19bd53ee4020c1e3370fa8a739fba71b6
ysansan <ysansan.tech@icloud.com>
2026-07-29 20:00:30 +0800

    chore: refactor lint-staged config, remove stale DESIGN.md

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,7 @@
  */
 export default {
   'apps/desktop/src/**/*.ts?(x)': () => 'tsc -p apps/desktop/tsconfig.json --noEmit',
-  'packages/**/*.{ts,js,jsx,tsx,vue}': 'eslint --fix --quiet',
   'apps/desktop/src/**/*.{ts,js,jsx,tsx}': 'eslint --fix --quiet',
+  'apps/web/src/**/*.{ts,js,jsx,tsx}': 'eslint --fix --quiet',
+  'packages/**/*.{ts,js,jsx,tsx}': 'eslint --fix --quiet',
 }
